954775b9aa66a81430a5db01ed8da30f358dd95f
Evelyn Lopez <evelyn.lopezilla@gmail.com>
2023-07-16 20:13:12 -0700

    Creating and Computing Between Angular Components
    - Created event-thumbnail component
    - Created events-list component
    - Created navbar component
    - Added bindings to display multiple event thumbnail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   
@@ -9,7 +12,10 @@ import { EventsAppComponent } from './events-app.component';
     BrowserModule
   ],
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   bootstrap: [EventsAppComponent]
 })
